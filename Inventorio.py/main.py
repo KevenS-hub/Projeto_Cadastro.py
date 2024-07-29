d4acea4f84ded5c586391cf79961c8df6a180d31
Keven Silveira <kevensilveira377@gmail.com>
2024-07-29 15:59:12 -0300

    Segundo Commit

---
+++ Inventorio.py/main.py
@@ -0,0 +1,53 @@
+from tkinter import *
+from tkinter import Tk, StringVar, ttk
+
+from PIL import Image, ImageTk
+
+# Cores
+
+co0 = "#2e2d2b"  # Preta
+co1 = "#feffff"  # branca
+co2 = "#4fa882"  # verde
+co3 = "#38576b"  # valor
+co4 = "#403d3d"   # letra
+co5 = "#e06636"   # - profit
+co6 = "#038cfc"   # azul
+co7 = "#3fbfb9"   # verde
+co8 = "#263238"   # + verde
+co9 = "#e9edf5"   # + verde
+
+# Criando a janela
+
+janela = Tk()
+janela.title('')
+janela.geometry('900x600')
+janela.configure(background=co9)
+janela.resizable(width=FALSE, height=FALSE)
+
+style = ttk.Style(janela)
+style.theme_use("clam")
+
+# Criando frames
+
+frameCima = Frame(janela, width=1043, height=50, bg=co1, relief=FLAT)
+frameCima.grid(row=0, column=0)
+
+frameMeio = Frame(janela, width=1043, height=303, bg=co1, pady=20, relief=FLAT)
+frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+frameBaixo = Frame(janela, width=1043, height=300, bg=co1, pady=20, relief=FLAT)
+frameBaixo.grid(row=2, column=0, pady=0, padx=0, sticky=NSEW)
+
+
+# Abrir imagem
+app_img = Image.open('midias/inventorio.png')
+app_img = app_img.resize((45,45))
+app_img = ImageTk.PhotoImage(app_img)
+
+app_logo = Label(frameCima, image=app_img, text=' Inventário Doméstico', width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
+app_logo.place(x=0, y=0)
+
+
+janela.mainloop()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Projeto_Cadastro-Sistema.py
  Projeto curto para testar utilização de bancos de dados interativos.
+
+ Projeto consiste em uma paltaforma de cadastro para usuários. Feito usando banco de dados sqlite, linguagem Python, e frameworks variados.
+ Feito por: Keven Silveira.
