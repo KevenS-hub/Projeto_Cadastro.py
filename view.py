6ffef3db0536cf10636b1b2d6ff2fb0870f8085d
Keven Silveira <kevensilveira377@gmail.com>
2024-07-29 14:33:37 -0300

    First Commit

--- view.py
+++ view.py
@@ -2,25 +2,55 @@ import sqlite3 as lite
 
 con = lite.connect('dados.db')
 
-dados = ['vaso','sala de estar', 'vaso que comprei na Shein', 'Marca X', '25/07/2024', '100', 'xxxxx', 'c:imagens']
 
-# inserir dados
-with con:
-    cur = con.cursor()
-    query = "INSERT INTO inventorio(nome, local, descricao, marca, data_da_compra, valor_da_compra, serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
-    cur.execute(query,dados)
+# CRUD
+
+# Inserir dados
+def inserir_form(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO inventorio(nome, local, descricao, marca, data_da_compra, valor_da_compra, serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
+        cur.execute(query, i)
+
+# Atualizar dados
+def atualizar_(i):
+    with con:
+        cur = con.cursor()
+        query = "UPDATE inventorio SET nome=?, local=?, descricao=?, marca=?, data_da_compra=?, valor_da_compra=?, serie=?, imagem=? WHERE id=?"
+        cur.execute(query, i)
 
 
-ver_dados = []
+# deletar dados
+def deletar_form(i):
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM inventorio WHERE id=?"
+        cur.execute(query, i)
+
+
+# Visualizar dados
+def ver_form(i):
+    ver_dados = []
+    with con:
+        cur = con.cursor()
+        query = "SELECT * FROM inventorio"
+        cur.execute(query)
+
+        rows = cur.fetchall()
+        for row in rows:
+            ver_dados.append(row)
+
+    return ver_dados
+
 
 # visualizar dados
-with con:
-    cur = con.cursor()
-    query = "SELECT * FROM inventorio"
-    cur.execute(query)
+def ver_item(id):
+    ver_dados_individual = []
+    with con:
+        cur = con.cursor()
+        query = "SELECT * FROM inventorio WHERE id=?"
+        cur.execute(query, id)
 
-    rows = cur.fetchall()
-    for row in rows:
-        ver_dados.append(row)
-
-print(ver_dados)
+        rows = cur.fetchall()
+        for row in rows:
+            ver_dados_individual.append(row)
