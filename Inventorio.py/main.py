85bb2d032b40284c1d10b0893bf0d2614d209dcb
Keven Silveira <kevensilveira377@gmail.com>
2024-07-29 18:15:06 -0300

    Segundo Commit

--- Inventorio.py/main.py
+++ Inventorio.py/main.py
@@ -13,7 +13,6 @@ co4 = "#403d3d"   # letra
 co5 = "#e06636"   # - profit
 co6 = "#038cfc"   # azul
 co7 = "#3fbfb9"   # verde
-co8 = "#263238"   # + verde
 co9 = "#e9edf5"   # + verde
 
 # Criando a janela
@@ -38,15 +37,47 @@ frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 frameBaixo = Frame(janela, width=1043, height=300, bg=co1, pady=20, relief=FLAT)
 frameBaixo.grid(row=2, column=0, pady=0, padx=0, sticky=NSEW)
 
+# Frames Cima -----------------------------------
+
 
 # Abrir imagem
 app_img = Image.open('midias/inventorio.png')
 app_img = app_img.resize((45,45))
 app_img = ImageTk.PhotoImage(app_img)
 
-app_logo = Label(frameCima, image=app_img, text=' Inventário Doméstico', width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
+app_logo = Label(frameCima, image=app_img, text=' Ficha de Cadastro', width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
 app_logo.place(x=0, y=0)
 
+
+# Frame Meio ------------------------------------
+
+# criando entradas
+l_nome = Label(frameMeio, text='Nome', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_nome.place(x=10, y=10)
+
+e_nome = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_nome.place(x=130, y=11)
+
+
+l_local = Label(frameMeio, text="Sala/Área", height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_local.place(x=10, y=40)
+
+e_local = Entry(frameMeio, width=30, justify='left', relief='solid')
+e_local.place(x=130, y=41)
+
+l_descricao = Label(frameMeio, text="Descrição", height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_descricao.place(x=10, y=70)
+e_descricao = Entry(frameMeio, width=30, justify='left', relief="solid")
+e_descricao.place(x=130, y=71)
+
+l_model = Label(frameMeio, text="Marca/Modelo", height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_model.place(x=10, y=100)
+e_model = Entry(frameMeio, width=30, justify='left', relief="solid")
+e_model.place(x=130, y=101)
+
+
+
+# Frame Baixo -----------------------------------
 
 janela.mainloop()
 
